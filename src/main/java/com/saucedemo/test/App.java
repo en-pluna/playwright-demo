b2e17b176516464ea90f7a52879414a95ab712f3
Paulina Luna <Paulina.Luna@endava.com>
2025-05-14 10:47:48 -0300

    Primer test con Playwright + Java

---
+++ src/main/java/com/saucedemo/test/App.java
@@ -0,0 +1,14 @@
+package com.saucedemo.test;
+
+import com.microsoft.playwright.*;
+
+public class App {
+    public static void main(String[] args) {
+        try (Playwright playwright = Playwright.create()) {
+            Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+            Page page = browser.newPage();
+            page.navigate("https://www.saucedemo.com/");
+            System.out.println("Título de la página: " + page.title());
+        }
+    }
+}
