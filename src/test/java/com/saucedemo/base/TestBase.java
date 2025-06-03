927174203bf6f81c520db5486ed38f6680cc4be6
Paulina Luna <Paulina.Luna@endava.com>
2025-06-03 16:56:28 -0300

    Fix github actions

--- src/test/java/com/saucedemo/base/TestBase.java
+++ src/test/java/com/saucedemo/base/TestBase.java
@@ -16,8 +16,13 @@ public class TestBase {
 
     @BeforeEach
     public void setUp() {
+        boolean isCI = System.getenv("CI") != null;
         playwright = Playwright.create();
-        browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false).setSlowMo(50));
+        browser = playwright.chromium().launch(
+            new BrowserType.LaunchOptions()
+                .setHeadless(isCI) // Headless en CI, visible localmente
+                .setSlowMo(50)
+        );
         page = browser.newPage();
     }
 
